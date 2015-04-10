6d20412879d8b6655fd673f00af7dae68f952d9a
Artur Wronski <a.wronski@mgmoptima.com>
2015-04-10 18:07:29 +0200

    - shopCategory added to the auction builder

--- src/main/java/com/apwglobal/allegro/client/auctionbuilder/AuctionBuilder.java
+++ src/main/java/com/apwglobal/allegro/client/auctionbuilder/AuctionBuilder.java
@@ -21,6 +21,7 @@ public class AuctionBuilder {
 
     private String title;
     private int category;
+    private int shopCategory;
     private Duration duration = UNLIMITED;
     private SellType sellType = SHOP;
     private int qty = 1;
@@ -55,6 +56,11 @@ public class AuctionBuilder {
 
     public AuctionBuilder category(int category) {
         this.category = category;
+        return this;
+    }
+
+    public AuctionBuilder shopCategory(int shopCategory) {
+        this.shopCategory = shopCategory;
         return this;
     }
 
@@ -184,6 +190,7 @@ public class AuctionBuilder {
 
         fields.add(new NewAuctionField(1, Type.STRING, title));
         fields.add(new NewAuctionField(2, Type.INTEGER, category));
+        fields.add(new NewAuctionField(31, Type.INTEGER, shopCategory));
         fields.add(new NewAuctionField(4, Type.INTEGER, duration.getType()));
         fields.add(new NewAuctionField(29, Type.INTEGER, sellType.getType()));
         fields.add(new NewAuctionField(5, Type.INTEGER, qty));
