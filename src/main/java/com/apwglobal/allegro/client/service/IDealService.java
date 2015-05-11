d2354f6741f82c1b15a583b41c1f88dfa94ab921
Artur Wronski <a.wronski@mgmoptima.com>
2015-05-11 14:19:00 +0200

    - getDealsAfterEventId was added

--- src/main/java/com/apwglobal/allegro/client/service/IDealService.java
+++ src/main/java/com/apwglobal/allegro/client/service/IDealService.java
@@ -2,6 +2,7 @@ package com.apwglobal.allegro.client.service;
 
 import com.apwglobal.nice.domain.Deal;
 import retrofit.http.GET;
+import retrofit.http.Path;
 import retrofit.http.Query;
 
 import java.util.List;
@@ -11,7 +12,10 @@ public interface IDealService {
     @GET("/deals")
     List<Deal> getLastDeals(@Query("limit") int limit);
 
-    @GET("/deals/after")
-    List<Deal> getDealsAfter(@Query("transactionId") long transactionId);
+    @GET("/deals/after/transactionId/{transactionId}")
+    List<Deal> getDealsAfterTransactionId(@Path("transactionId") long transactionId);
+
+    @GET("/deals/after/eventId/{eventId}")
+    List<Deal> getDealsAfterEventId(@Path("eventId") long eventId);
 
 }
--- src/test/java/com/apwglobal/allegro/client/service/DealServiceTest.java
+++ src/test/java/com/apwglobal/allegro/client/service/DealServiceTest.java
@@ -4,8 +4,9 @@ import com.apwglobal.nice.domain.Deal;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.Optional;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -24,17 +25,35 @@ public class DealServiceTest extends AbstractIntegrationTest {
     }
 
     @Test
-    public void shouldReturnDealsAfter() {
-        List<Deal> lastDeals = client.getDealService().getLastDeals(100);
-        Optional<Deal> dealWithTransactionId = lastDeals
+    public void shouldReturnDealsAfterTransactionId() {
+        List<Deal> lastDeals = client.getDealService().getLastDeals(100)
                 .stream()
                 .filter(d -> d.getTransactionId().isPresent())
-                .reduce((a, b) -> b);
+                .collect(toList());
 
-        if (dealWithTransactionId.isPresent()) {
-            long transactionId = dealWithTransactionId.get().getTransactionId().get();
-            List<Deal> after = client.getDealService().getDealsAfter(transactionId);
+        if (!lastDeals.isEmpty()) {
+            Long minTransactionId = lastDeals
+                    .stream()
+                    .min(comparing(deal -> deal.getTransactionId().get()))
+                    .get()
+                    .getTransactionId()
+                    .get();
+
+            List<Deal> after = client.getDealService().getDealsAfterTransactionId(minTransactionId);
             assertTrue(lastDeals.size() > after.size());
+        }
+    }
+
+    @Test
+    public void shouldReturnDealsAfterEventId() {
+        List<Deal> lastDeals = client.getDealService().getLastDeals(100)
+                .stream()
+                .sorted(comparing(Deal::getEventId))
+                .collect(toList());
+
+        if (lastDeals.size() > 1) {
+            List<Deal> after = client.getDealService().getDealsAfterEventId(lastDeals.get(0).getEventId());
+            assertTrue(lastDeals.size() - 1 == after.size());
         }
     }
 
